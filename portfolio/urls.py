20cb859bc1c680e9c9f30ca6075c2b4065fb02ae
Garrett Kadillak <gkadillak@gmail.com>
2016-05-07 22:58:24 -0700

    Shim to serve static files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('portfolio.blog.api.urls', namespace='blog')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
