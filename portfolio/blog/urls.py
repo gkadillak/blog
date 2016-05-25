cdd90674e1836352e4442454afc3c08026cae91c
Garrett Kadillak <gkadillak@gmail.com>
2016-05-25 07:41:56 -0700

    View unpublished entries

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from portfolio.blog.views import (blog, about, portfolio,
-                                  entry, entries_with_given_tag, search_results)
+                                  entry, entries_with_given_tag, search_results,
+                                  entry_preview, unpublished_entries)
 
 
 urlpatterns = [
@@ -8,6 +9,8 @@ urlpatterns = [
     url(r'^about/?$', about, name='about'),
     url(r'^porfolio/?$', portfolio, name='portfolio'),
     url(r'^entry/(?P<pk>\d+)/?$', entry, name='entry'),
-    url(r'^entries/(?P<tag_name>\w+)/?$', entries_with_given_tag, name='entries_with_given_tag'),
-    url(r'^entries/search/?', search_results, name='search_results'),
+
+    url(r'^entries/search/?$', search_results, name='search_results'),
+    url(r'^entries/unpublished/?$', unpublished_entries, name='unpublished_entries'),
+        url(r'^entries/(?P<tag_name>\w+)/?$', entries_with_given_tag, name='entries_with_given_tag'),
 ]
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 
 
 def blog(request):
-    entry_list = Entry.objects.all()
+    entry_list = Entry.objects.filter(published=True)
     paginator = Paginator(entry_list, 10)
 
     page = request.GET.get('page')
@@ -27,7 +27,7 @@ def blog(request):
 
 
 def entries_with_given_tag(request, tag_name):
-    entries = Entry.objects.filter(tag__name=tag_name)
+    entries = Entry.objects.filter(tags__name=tag_name)
 
     ctx = {
         'entries': entries
@@ -64,4 +64,36 @@ def search_results(request, search_string):
     ctx = {
         'entries': entries
     }
+    return render(request, 'blog/entries.html', ctx)
+
+
+def entry_preview(request, pk):
+    entry = get_object_or_404(Entry, id=pk)
+
+    ctx = {
+        'entry': entry,
+    }
+
+    return render(request, 'blog/entry.html', ctx)
+
+
+def unpublished_entries(request):
+
+    entry_list = Entry.objects.filter(published=False)
+    paginator = Paginator(entry_list, 10)
+
+    page = request.GET.get('page')
+    try:
+        entries = paginator.page(page)
+    except PageNotAnInteger:
+        # If page not an integer, deliver first page
+        entries = paginator.page(1)
+    except EmptyPage:
+        # If no more results, deliver last of results
+        entries = paginator.page(paginator.num_pages)
+
+    ctx = {
+        'entries': entries,
+    }
+
     return render(request, 'blog/entries.html', ctx)
