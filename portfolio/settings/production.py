9dc4f679f00e48f59d7c1613f44c9e618c5a82e0
Garrett Kadillak <gkadillak@gmail.com>
2016-05-31 14:09:42 -0700

    log error levels in prod

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -6,6 +6,10 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'portfolio.settings.production'
+ERROR_LEVEL = 'ERROR'
+
+LOGGING['loggers']['django']['level'] = ERROR_LEVEL
+LOGGING['handlers']['file']['level'] = ERROR_LEVEL
 
 DATABASES = {
     'default': {
