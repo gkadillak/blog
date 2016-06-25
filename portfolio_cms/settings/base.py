0a5a2c53b1f1cac1daa071aaa99e535b3300daa4
Garrett Kadillak <gkadillak@gmail.com>
2016-06-25 10:24:49 -0700

    Revert "Remove print from base settings"
    This reverts commit ff01ffc1a276b6f4b88d67db90669b7ed5477f08.

--- portfolio_cms/settings/base.py
+++ portfolio_cms/settings/base.py
@@ -15,6 +15,7 @@ import os
 from unipath import Path
 
 BASE_DIR = Path(__file__).ancestor(3)
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
