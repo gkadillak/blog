df3985b0acc76c7d3578bec1d550c64661d45874
Garrett Kadillak <gkadillak@gmail.com>
2016-07-07 07:53:31 -0700

    Update production settings.

--- portfolio_cms/settings/production.py
+++ portfolio_cms/settings/production.py
@@ -1,10 +1,16 @@
 from .base import *
+import os
 
 DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio_cms',
+        'USER': 'blog-user',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
