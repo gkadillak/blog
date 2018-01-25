7531c195b041dda56b5fe2a537d3e9e6422c4cfa
Garrett Kadillak <gkadillak@gmail.com>
2018-01-24 22:03:08 -0800

    Remove unresolved module in parent routes

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/?', admin.site.urls),
     url(r'^', include('portfolio.urls', namespace='portfolio')),
-    url(r'^wiki-edits/', include('wiki_edits.urls', namespace='wiki_edits')),
 ]
-
