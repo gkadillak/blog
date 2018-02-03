0c59371ded435fb6e77cb36ee6587b3becb98691
Garrett Kadillak <gkadillak@gmail.com>
2018-02-03 14:12:39 -0800

    Django 1.9.8 -> 2.0.2, mandated url updates, add post created date

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from .views import bio, portfolio, home, post, ssl_challenge
 
 
+app_name = 'portfolio'
+
 urlpatterns = [
     url(r'^bio/?$', bio, name='bio'),
     url(r'^portfolio/?$', portfolio, name='projects'),
     url(r'^post/(?P<slug>[-\w]+)/?$', post, name='post'),
-    url(r'.well-known/acme-challenge/(?P<challenge>[\S]+)', ssl_challenge, name='ssl_challenge'),
     url(r'^$', home, name='home'),
 ]
